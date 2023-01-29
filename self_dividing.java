85e6a8146de6194a3daecfe0fcad6a3de6fdfb08
theyug <116864783+theyug@users.noreply.github.com>
2023-01-29 17:40:38 +0530

    Add files via upload

---
+++ self_dividing.java
@@ -0,0 +1,27 @@
+public class Main {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = left;i<=right;i++){
+            if(isDividing(i)){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+    public static boolean isDividing(int n){
+        if(n<=0)
+            return false;
+        int rem = 0;
+        int temp = n;
+        while(n!=0){
+            rem = n%10;
+            if(rem==0 || temp%rem != 0){
+                return false;
+            }
+            n/=10;
+        }
+        return true;
+    }
+}
+    
+}
